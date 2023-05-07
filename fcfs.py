96db9363c89335a97c81db41be46df20ccb29f30
pedrohdsouza <phsouzaa2@gmail.com>
2023-05-07 15:43:59 -0300

    Adicionando Plot da FCFS

--- fcfs.py
+++ fcfs.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import matplotlib.pyplot as plt
 
 def calculateStartEndTime(processBurstTime, elapsedTime):
@@ -14,9 +15,26 @@ def calculateStartEndTime(processBurstTime, elapsedTime):
     end = start + processBurstTime
     return start, end
 
-def addProcessToGranttChart(processExecution):
+def addProcessToGanttChart(processExecution):
     # Logic to generate Gantt Chart
     print('Generating Gantt Chart...')
+    processList.append(processExecution)
+
+def plotGanttChart(processList):
+    processIdList = [item[0] for item in processList]
+    processStartList = [item[1] for item in processList]
+    processEndList = [item[2] for item in processList]
+
+    df = pd.DataFrame({'Process': processIdList,
+                'start':processStartList,
+                'end': processEndList,
+                })
+    
+    df['process_duration'] = df['end'] - df['start']
+    print(df)
+
+    plt.barh(y=df['Process'], width=df['process_duration'], left=df['start'])
+    plt.show()  
 
 def executeFCFS(readyQeue):
     '''
@@ -31,14 +49,17 @@ def executeFCFS(readyQeue):
         processBurstTime = process[1]
         start, end = calculateStartEndTime(processBurstTime, elapsedTime)
         processExecution = (processId, start, end)
-        addProcessToGranttChart(processExecution)
+        addProcessToGanttChart(processExecution)
         
         elapsedTime += processBurstTime
+    
+    plotGanttChart(processList)
 
 if __name__ == '__main__':
     numberProcess = int(input('How many process do you want to simulate FCFS algorithm?\t'))
     
     readyQeue = []
+    processList = []
 
     for process in range(numberProcess):
         burstTime = int(input(f'What is the burst time of the process {process+1}?\t'))
